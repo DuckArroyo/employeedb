66f7d7118797210e5cd001a2d06f68b501a7602f
Rafael "Duck" Arroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-11-09 16:30:52 -0800

    worked on new server.js setup

--- db/schema.sql
+++ db/schema.sql
@@ -18,15 +18,16 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER,
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
     manager_id INTEGER,
-    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES manager(id),department_id INTEGER,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL,
+    department_id INTEGER,
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id),
 );
 
 CREATE TABLE role (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    id INTEGER AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL(6,2),
-    department_id INTEGER NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER,
 );
--- server.js
+++ server.js
@@ -1,3 +1,53 @@
 //!From tutor we can delete this file
 
 //Restarted from tutor's advice
+
+//sql
+const mysql = require("mysql2");
+
+//inquirer
+const inquirer = require("inquirer");
+
+const consoleTable = require("console.table");
+
+require(".env").config();
+
+const connect = mysql.createConnection({
+  database: "DB_NAME",
+  host: "DB_HOST",
+  user: "DB_USER",
+  password: "DB_PW",
+});
+
+connect.connect((err) => {
+  if (err) throw err;
+  console.log("Connection stablished");
+});
+
+const prompt = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "choices",
+        message: "What action would you like to perform?",
+        choices: [
+          "view all departments",
+          "view all roles",
+          "view all employees",
+          "add a department",
+          "add a role",
+          "add an employee",
+          "update an employee role",
+        ],
+      },
+    ])
+    .then((answers) => {
+      //Switches code
+    });
+};
+
+viewDepartments = () => {
+  console.log("Viewing departments");
+  const sql = `SELECT department.id AS id, department.name AS department FROM department`;
+};
