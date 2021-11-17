7f1816bc4cd1b8f3edb60c312cc2b9199395c9ba
Rafael <developerduckarroyo@gmail.com>
2021-11-17 08:03:03 -0800

    sorting PC files

--- db/schema.sql
+++ db/schema.sql
@@ -1,28 +1,11 @@
 DROP TABLE IF EXISTS employee;
-DROP TABLE IF EXISTS department;
 DROP TABLE IF EXISTS role; 
+DROP TABLE IF EXISTS department;
+
 
 CREATE TABLE department (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL,
-);
-
-CREATE TABLE manager (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    employee_id INTEGER, -- Q, 
-    CONSTRAINT fk_employee FOREIGN KEY (employee_id) REFERENCES employee(id),
-)
-
-CREATE TABLE employee (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER,
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
-    manager_id INTEGER,
-    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL,
-    department_id INTEGER,
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id),
 );
 
 CREATE TABLE role (
@@ -31,3 +14,14 @@ CREATE TABLE role (
     salary DECIMAL NOT NULL,
     department_id INTEGER,
 );
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    manager_id INTEGER,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES manager(id),
+);
+
